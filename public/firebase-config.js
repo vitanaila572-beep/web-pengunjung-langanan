6792c0314fcc480437bd41d65b13e9724478fae3
vitanaila572-beep <vitanaila572@gmail.com>
2026-09-02 02:53:00 +0700

    Update firebase-config.js

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,13 +1,14 @@
-// Import Firebase SDK
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "PROJECT.firebaseapp.com",
-  projectId: "PROJECT_ID",
-  storageBucket: "PROJECT.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID"
+  apiKey: "AIzaSyDnSOhoYWyaSLxS8uUaJNknzQGeiAeaV7E",
+  authDomain: "nexora-719b2.firebaseapp.com",
+  projectId: "nexora-719b2",
+  storageBucket: "nexora-719b2.firebasestorage.app",
+  messagingSenderId: "328409390682",
+  appId: "1:328409390682:web:d35c7da65acf70de5bc263"
 };
 
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
